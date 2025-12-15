89391bc369868b5f4a04f5317cd128bd479b420c
aggrabbah <hello@aggrabbah.dev>
2025-12-15 22:28:50 +0100

    [TASK] Add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,17 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'WorkOS Auth',
+    'description' => 'This extensions allows you to handle auth through WorkOS',
+    'category' => 'plugin',
+    'author' => 'Aggrabbah',
+    'author_email' => 'hello@aggrabbah.dev',
+    'author_company' => 'aggrabbah.dev',
+    'state' => 'stable',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+        ]
+    ],
+];
